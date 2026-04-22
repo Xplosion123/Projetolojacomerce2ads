648b6799066eab4689590522dbf47de2d8d23555
Henrique <henrimelo06@gmail.com>
2026-04-22 17:24:47 -0300

    feat: Script do banco de dados

---
+++ Script.sql
@@ -0,0 +1,24 @@
+-- Criando DataBase
+CREATE DATABASE dbProjetoLoja;
+
+-- Usando o banco de dados
+USE dbProjetoLoja;
+
+-- CRIANDO AS TABELAS DE BANCO DE DADOS
+CREATE TABLE tb_Usuario(
+Id int primary key auto_increment,
+Nome varchar(50) not null,
+Email varchar(50) not null,
+Senha varchar(250) not null,
+Nivel varchar(50) not null
+
+);
+
+-- CONSULTANDO A TABELA DO BANCO DE DADOS 
+
+SELECT * FROM tb_Usuario;
+
+-- INSERINDO DADOS NA TABELA DO BANCO DE DADOS 
+
+INSERT INTO tb_Usuario(Nome,Email,Senha, Nivel) VALUES
+('Administrador','admin@gmail.com','123456','Admin');
