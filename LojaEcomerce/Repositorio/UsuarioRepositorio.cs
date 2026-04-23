02dc0dbc4b2ef5df8ba31e410798eeda4192e6f3
Henrique <henrimelo06@gmail.com>
2026-04-23 16:46:43 -0300

    feat: codificando repositorio e interface

---
+++ LojaEcomerce/Repositorio/UsuarioRepositorio.cs
@@ -0,0 +1,22 @@
+﻿using LojaEcomerce.Models;
+using MySql.Data.MySqlClient;
+
+namespace LojaEcomerce.Repositorio
+{
+    public class UsuarioRepositorio
+    {
+        //variavel privada somente leitura
+        //receber a conexão do banco de dados
+        private readonly string _connectionString;
+
+        public UsuarioRepositorio(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("Conexao");
+        }
+        // metodo para validar o login
+        public LoginViewModel validar(string email, string senha)
+        {
+            using var conn = new MySqlConnection(_connectionString);
+        }
+    }
+}
